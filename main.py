50d6753c4a80a6ce3ac13f843831e95bca036b17
Alireza <alirezarezaei8080@gmail.com>
2022-08-19 14:02:40 +0430

    basic inline button funcitonality

--- main.py
+++ main.py
@@ -17,8 +17,25 @@ def start(update,context):
     reply_markup = InlineKeyboardMarkup(menu)
     context.bot.send_message(chat_id=update.effective_chat.id, text="How can I be of service?",reply_markup=reply_markup)
 
+def define_task(update,context):
+    query = update.callback_query
+    query.answer()
+    if not context.user_data.get("state"):
+        context.user_data["state"] = "define_title"
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Let's create new Task\n What would be the title?")    
+
+def show_tasks(update,context):
+    query = update.callback_query
+    query.answer()
+    if not context.user_data.get("state"):
+        context.bot.send_message(chat_id=update.effective_chat.id, text="You pressed show button")
+
+
 start_handler = CommandHandler("start",start)
+
 dispatcher.add_handler(start_handler)
+dispatcher.add_handler(CallbackQueryHandler(define_task,pattern="1"))
+dispatcher.add_handler(CallbackQueryHandler(show_tasks,pattern="2"))
 
 updater.start_polling()
 
