d4ecfcc1094cde43b8be880a37feb542a9bc2495
Alireza <alirezarezaei8080@gmail.com>
2022-08-19 17:35:59 +0430

    database interface added with showing tasks

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python
 
+import pickle
+import sqlite3
 import datetime
 
 from telegram import Update , InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CallbackContext, CommandHandler, CallbackQueryHandler, MessageHandler, Filters
 
 months = {"January": 1, "February":2, "March": 3, "April":4, "May": 5, "June": 6, "July":7, "August":8, "September":9, "October":10, "November":11, "December": 12}
-request_kargs = {"proxy_url":"http://127.0.0.1:62178"} # Hard-coded only for test
+request_kargs = {"proxy_url":"http://127.0.0.1:50886"} # Hard-coded only for test
 
 updater = Updater(token="5652106016:AAFUm4917Re2TmHpL1IZc1lPGgmPv49K3Cg", request_kwargs=request_kargs)
 
 dispatcher = updater.dispatcher
+
+def setup():
+    db = sqlite3.connect("xobot.db")
+    cur = db.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS Tasks (User INTEGER, Title TEXT, dateobj BLOB)")
+    db.commit()
+    db.close()
 
 def start(update,context):
     menu = [
@@ -29,12 +38,20 @@ def define_task(update,context):
         context.bot.send_message(chat_id=update.effective_chat.id, text="Let's create new Task\n What would be the title?")    
 
 def show_tasks(update,context):
+    db = sqlite3.connect("xobot.db")
+    cur = db.cursor()
     query = update.callback_query
     query.answer()
+    cur.execute("SELECT * FROM Tasks WHERE User={}".format(update.effective_user.id))
+    results = cur.fetchall()
+    for result in results:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Title: {}\n Due Date: {}".format(result[1],pickle.loads(result[2])))
     if not context.user_data.get("state"):
         context.bot.send_message(chat_id=update.effective_chat.id, text="You pressed show button")
 
 def text_handler(update,context):
+    db = sqlite3.connect("xobot.db")
+    cur = db.cursor()
     state = context.user_data.get("state")
     if state:
         if state == "define_title":
@@ -50,11 +67,16 @@ def text_handler(update,context):
             due_month = months[due_date[2]]
             due_year = int(due_date[3])
             due = datetime.datetime(due_year,due_month,due_day,hour=due_hour,minute=due_minute)
-            context.user_data["task"]["date"] = due
-            context.user_data["task"]["id"] = hash(due)
-            context.bot.send_message(chat_id=update.effective_chat.id, text="Task created with ID {}".format(context.user_data["task"]["id"]))
+            cur.execute("INSERT INTO Tasks VALUES (?,?,?)",(update.effective_user.id,context.user_data["task"]["title"],pickle.dumps(due)))
+            db.commit()
+            db.close()
+            context.user_data.pop("state")
+            context.user_data.pop("task")
+            context.bot.send_message(chat_id=update.effective_chat.id, text="Task created with ID {}".format(hash(due)))
 
 if __name__ == "__main__":
+
+    setup()
 
     start_handler = CommandHandler("start",start)
     msg_handler = MessageHandler(Filters.text & (~Filters.command),text_handler)
