311f15bc8a7c3496496480422df6008c683ff8ee
Alireza <alirezarezaei8080@gmail.com>
2022-08-19 11:24:27 +0430

    added inline keyboard as menu

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
-from telegram import Update
-from telegram.ext import Updater, CallbackContext, CommandHandler
+from telegram import Update , InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CallbackContext, CommandHandler, CallbackQueryHandler
 
 request_kargs = {"proxy_url":"http://127.0.0.1:62178"} # Hard-coded only for test
 
@@ -10,9 +10,16 @@ updater = Updater(token="5652106016:AAFUm4917Re2TmHpL1IZc1lPGgmPv49K3Cg", reques
 dispatcher = updater.dispatcher
 
 def start(update,context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to Xobot")
+    menu = [
+        [InlineKeyboardButton("Define Task",callback_data="1"),
+        InlineKeyboardButton("Show Tasks",callback_data="2")]
+    ]
+    reply_markup = InlineKeyboardMarkup(menu)
+    context.bot.send_message(chat_id=update.effective_chat.id, text="How can I be of service?",reply_markup=reply_markup)
 
 start_handler = CommandHandler("start",start)
 dispatcher.add_handler(start_handler)
 
 updater.start_polling()
+
+updater.idle()
